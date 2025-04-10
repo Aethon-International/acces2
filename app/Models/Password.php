5880bc4f6cb8ede477024bfee74f9ed9b83deb40
safeer <safeeraethon@gmail.com>
2025-04-10 08:36:54 +0500

    acces2 done all changind working final testin done

---
+++ app/Models/Password.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Password extends Model
+{
+    use HasFactory;
+
+    protected $table = 'passwords';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'item_id',
+        'name',
+        'note',
+       
+    ];
+
+
+    public function items()
+    {
+        return $this->hasMany(PasswordItem::class, 'password_id', 'id');
+    }
+}
